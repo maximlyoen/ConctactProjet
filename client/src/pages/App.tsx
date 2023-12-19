b37904456bf640d3df99e6c57d8aa09655a3f24b
tiballion <timothee.ballion.33@gmail.com>
2023-12-19 13:56:07 +0100

    [EDIT] - Not using useFetch anymore, improved typing

--- client/src/pages/App.tsx
+++ client/src/pages/App.tsx
@@ -1,45 +1,55 @@
-import { Header } from '../components/';
-import useFetch from '../hooks/useFetch';
+import { Header, EntrepriseCard } from '../components/';
+import { useState, useEffect } from 'react';
 
 export const App = () => {
   type Entreprise = {
     id: number;
     name: string;
     logo: string;
-  }
-
-  type Response = {
-    entreprises: Entreprise[]
-  }
-
-  type ApiResponse = {
-    response: Response | null;
-    loading: boolean;
-    error: Error | null;
   };
 
-  const { response, loading, error }: ApiResponse = useFetch('http://localhost:3000/api/entreprises');
+  type ApiResponse = {
+    entreprises: Entreprise[];
+  };
 
-  if (loading) {
-    return <p>Loading...</p>
-  }
+  const [response, setResponse] = useState<ApiResponse | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
 
-  if (error) {
-    return <p>Error: {error}</p>
-  }
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await fetch('http://localhost:3000/api/entreprises');
+        const data = await res.json();
+        setResponse(data);
+        setLoading(false);
+      } catch (error) {
+        setError('Error fetching data');
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
 
   return (
     <>
       <div>
         <Header />
         <h1>Posts</h1>
+
+        {loading && (
+          <div>Loading...</div>
+        )}
+
+        {error && (
+          <div>Error!</div>
+        )}
+        
         <ul>
-          {/*entreprises.map((entreprise: Entreprise) => (
+          {response && response.entreprises.map((entreprise: Entreprise) => (
             <EntrepriseCard key={entreprise.id} entreprise={entreprise} />
-          ))*/}
-          <pre>
-            {JSON.stringify(response, null, 2)}
-          </pre>
+          ))}
         </ul>
       </div>
     </>
