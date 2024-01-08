f98ddde62af5424579549860d781812c01f7307c
tiballion <timothee.ballion.33@gmail.com>
2024-01-08 16:36:06 +0100

    link bd

--- client/src/pages/App.tsx
+++ client/src/pages/App.tsx
@@ -1,13 +1,38 @@
 import { Header } from "../components";
 import { useAuth } from "../hooks/useAuth";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 export const App = () => {
   const { token, login } = useAuth();
+  const navigate = useNavigate();
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
 
   const handleLogin = () => {
     const jwtToken = 'your_jwt_token_here';
     login(jwtToken);
   }
+
+  useEffect(() => {
+    if (!token) navigate("/");
+
+    const fetchData = async () => {
+      try {
+        const res = await axios.get<any>(`http://185.212.227.8:3002/api/contacts/1`);  
+        const data = res.data;
+        setData(data);
+        setLoading(false);
+      } catch (error) {
+        setError(null);
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
 
   return (
     <>
@@ -22,6 +47,11 @@ export const App = () => {
             <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold px-4 py-2 m-2 rounded" onClick={handleLogin}>Se connecter</button>
           </>
           )
+          }
+          {
+            data && <pre>
+              {JSON.stringify(data, null, 2)}
+            </pre>
           }
       </div>
     </>
