aa2507b72ee1b29838c519c6692a36a893efc6db
dlevrault <damien.levrault@etu.u-bordeaux.fr>
2024-01-09 13:49:02 +0100

    api avec jwt

--- server/app.js
+++ server/app.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const axios = require('axios');
 const cors = require('cors');
-const { fetchDataForAllPersonnes, fetchDataForEntreprise, testQuery, insert} = require('./functions');
+
+const { avoirTousLestags, avoirTagsContacts, avoirContacts, avoirContact, avoirContactAvecMail, ajouterContact,
+modifierContact, supprimerContact, avoirContacstEntreprises, avoirListeEntreprises, avoirListeEntrepriseParId,
+avoirListeEntrepriseParNom, ajouterEntreprise, modifierEntreprise, supprimerEntreprise, avoirContactsEntreprise,
+avoirTags, avoirTagsId} = require('./functions');
+
 const { generateAccessToken } = require('./authentication');
 const app = express();
 const port = process.env.PORT || 3000;
@@ -23,122 +28,285 @@ app.post('/login', (req, res) => {
   res.json({acccesToken: acccesToken});
 });
 
-app.get('/entreprises/:id', async (req, res) => {
-    const authHeader = req.headers['authorization'];
-    const token = authHeader && authHeader.split(' ')[1];
-    if (token == null) return res.sendStatus(401);
 
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-      if (err) return res.sendStatus(403)
-    })
-    const result = await testQuery(`${req.params.id}`).then((result) => {
-      console.log(result);
-    });
-    //entreprise = {"entreprise" : result[0]};
-    // console.log(result);
-    // return res.json(result);
-  
+app.get('/api/contacts/tags',   async (req, res) => {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+  if (token == null) return res.sendStatus(401);
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    if (err) return res.sendStatus(403)
+  })
+  await avoirTousLestags().then((r) => {
+    res.json(r);
+  });
 });
 
-app.put('/api/contacts/add/', async (req, res) => {
-  try {
-    const {
-      ID_ENTREPRISE,
-      MAIL,
-      MOBILE,
-      NOM,
-      PRENOM,
-      DESCRIPTION,
-      RH
-    } = req.body;
-    
-    // const conn = await pool.getConnection();
-    // const result = await conn.query(
-    sql = 'INSERT INTO CONTACTS (ID_ENTREPRISE, MAIL, MOBILE, NOM, PRENOM, DESCRIPTION, RH) VALUES (?, ?, ?, ?, ?, ?, ?)'
-    insert(sql, [ID_ENTREPRISE, MAIL, MOBILE, NOM, PRENOM, DESCRIPTION, RH])
-      return res.sendStatus(200);
-    // );
+app.get('/api/contacts/:id/tags',   async (req, res) => {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+  if (token == null) return res.sendStatus(401);
 
-    // conn.release();
-
-    // Convertir les valeurs BigInt en chaînes dans la réponse JSON
-    // const insertId = result.insertId.toString();
-    // res.json({ message: 'Contact ajouté avec succès', insertId });
-  } catch (err) {
-    console.error(err);
-    res.status(500).send('Erreur serveur');
-  }
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    if (err) return res.sendStatus(403)
+  })
+  await avoirTagsContacts(`${req.params.id}`).then((r) => {
+    res.json(r);
+  });
 });
 
-// Example route for handling /api/entreprises
+app.get('/api/contacts',   async (req, res) => {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+  if (token == null) return res.sendStatus(401);
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    if (err) return res.sendStatus(403)
+  })
+  await avoirContacts().then((r) => {
+    res.json(r);
+  });
+});
+
+// Récupérer un contact par son ID
+app.get('/api/contacts/:id',   async (req, res) => {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+  if (token == null) return res.sendStatus(401);
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    if (err) return res.sendStatus(403)
+  })
+  await avoirContact(`${req.params.id}`).then((r) => {
+    res.json(r);
+  });
+});
+
+// Récupérer un contact par son email
+app.get('/api/contacts/mail/:email',   async (req, res) => {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+  if (token == null) return res.sendStatus(401);
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    if (err) return res.sendStatus(403)
+  })
+  await avoirContactAvecMail(`${req.params.email}`).then((r) => {
+    res.json(r);
+  });
+});
+
+//ajouter contact
+app.put('/api/contacts/add/',   async (req, res) => {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+  if (token == null) return res.sendStatus(401);
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    if (err) return res.sendStatus(403)
+  })
+  const {
+    ID_ENTREPRISE,
+    MAIL,
+    MOBILE,
+    NOM,
+    PRENOM,
+    DESCRIPTION,
+    RH
+  } = req.body;
+  await ajouterContact(ID_ENTREPRISE, MAIL, MOBILE, NOM, PRENOM, DESCRIPTION, RH).then((r) => {
+    res.json(r);
+  });
+});
+
+//modifier contact
+app.patch('/api/contacts/mod/:id/',   async (req, res) => {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+  if (token == null) return res.sendStatus(401);
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    if (err) return res.sendStatus(403)
+  })
+  const {
+    ID_ENTREPRISE,
+    MAIL,
+    MOBILE,
+    NOM,
+    PRENOM,
+    DESCRIPTION,
+    RH
+  } = req.body;
+  await modifierContact(ID_ENTREPRISE, MAIL, MOBILE, NOM, PRENOM, DESCRIPTION, RH, `${req.params.id}`).then((r) => {
+    res.json({message:"contact modifié"});
+  });
+});
+
+//supprimer contact
+app.delete('/api/contacts/del/:id', async (req, res) => {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+  if (token == null) return res.sendStatus(401);
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    if (err) return res.sendStatus(403)
+  })
+  await supprimerContact(`${req.params.id}`).then((r) => {
+    res.json({message:"contact supprimé"});
+  });
+});
+
+// affiche la liste des contacts avec leur entreprise associée
+app.get('/api/contactsentreprises', async (req, res) => {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+  if (token == null) return res.sendStatus(401);
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    if (err) return res.sendStatus(403)
+  })
+  await avoirContacstEntreprises().then((r) => {
+    res.json({r});
+  });
+});
+
+// avoir la liste des entreprises
 app.get('/api/entreprises', async (req, res) => {
-  try {
-    // Make a request to localhost:3001/entreprises
-    const response = await axios.get('http://localhost:3001/entreprises');
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+  if (token == null) return res.sendStatus(401);
 
-    // Extract entreprises from the response data
-    const entreprises = response.data;
-
-    // Respond with the entreprises in JSON format
-    res.json({ entreprises });
-  } catch (error) {
-    console.error('Error fetching posts:', error.message);
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    if (err) return res.sendStatus(403)
+  })
+  await avoirListeEntreprises().then((r) => {
+    res.json({r});
+  });
 });
 
+// avoir une entreprise par id
 app.get('/api/entreprises/:id', async (req, res) => {
-  try {
-    // Make a request to localhost:3001/entreprises/:id
-    const response = await axios.get(`http://localhost:3001/entreprises/${req.params.id}`);
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+  if (token == null) return res.sendStatus(401);
 
-    // Extract entreprises from the response data
-    const entreprise = response.data;
-
-    // Respond with the entreprises in JSON format
-    res.json({ entreprise });
-  } catch (error) {
-    console.error('Error fetching posts:', error.message);
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    if (err) return res.sendStatus(403)
+  })
+  await avoirListeEntrepriseParId(`${req.params.id}`).then((r) => {
+    res.json({r});
+  });
 });
 
-app.get('/api/personnes', async (req, res) => {
-  try {
-    fetchDataForAllPersonnes().then(personnes => {
-      res.json({personnes});
-    });
-  } catch (error) {
-    console.error('Error fetching posts:', error.message);
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
+// avoir une entreprise par nom
+app.get('/api/entreprises/nom/:nom', async (req, res) => {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+  if (token == null) return res.sendStatus(401);
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    if (err) return res.sendStatus(403)
+  })
+  await avoirListeEntrepriseParNom(`${req.params.nom}`).then((r) => {
+    res.json({r});
+  });
 });
 
-app.get('/api/personnes/:id', async (req, res) => {
-  try {
-    // Make a request to localhost:3001/personnes/:id
-    const response = await axios.get(`http://localhost:3001/personnes/${req.params.id}`);
-    // Extract personnes from the response data
-    const personne = response.data;
+//ajouter une entreprise
+app.put('/api/entreprises/add/',   async (req, res) => {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+  if (token == null) return res.sendStatus(401);
 
-    const resultsArray = await fetchDataForEntreprise(personne.entreprise);
-    const result = {
-      ...personne,
-      entreprise: resultsArray
-    };
-
-    // Respond with the personnes in JSON format
-    res.json({ result });
-  } catch (error) {
-    console.error('Error fetching posts:', error.message);
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    if (err) return res.sendStatus(403)
+  })
+  const {
+    NOM,
+    IMAGE
+  } = req.body;
+  await ajouterEntreprise(NOM, IMAGE).then((r) => {
+    res.json(r);
+  });
 });
 
-// Catch-all route to serve React app or handle other routes
-app.get('*', (req, res) => {
-  // Your logic to handle other routes or serve React app
-  res.send('Hello, this is the catch-all route!');
+//modifier une entreprise
+app.patch('/api/entreprises/mod/:id/',   async (req, res) => {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+  if (token == null) return res.sendStatus(401);
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    if (err) return res.sendStatus(403)
+  })
+  const {
+    NOM,
+    IMAGE,
+  } = req.body;
+
+  await modifierEntreprise(NOM, IMAGE, `${req.params.id}`).then((r) => {
+    res.json(r);
+  });
+});
+
+//supprimer une entreprise
+app.delete('/api/entreprises/del/:id', async (req, res) => {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+  if (token == null) return res.sendStatus(401);
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    if (err) return res.sendStatus(403)
+  })
+
+  await supprimerEntreprise(`${req.params.id}`).then((r) => {
+    res.json(r);
+  });
+});
+
+// avoir les contacts d'une entreprise
+app.get('/api/entreprises/:id/contacts',   async (req, res) => {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+  if (token == null) return res.sendStatus(401);
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    if (err) return res.sendStatus(403)
+  })
+
+  await avoirContactsEntreprise(`${req.params.id}`).then((r) => {
+    res.json(r);
+  });
+});
+
+// avoir les tags
+app.get('/api/tags',   async (req, res) => {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+  if (token == null) return res.sendStatus(401);
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    if (err) return res.sendStatus(403)
+  })
+
+  await avoirTags().then((r) => {
+    res.json(r);
+  });
+});
+
+// avoir les tags par id
+app.get('/api/tags/:id',   async (req, res) => {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+  if (token == null) return res.sendStatus(401);
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    if (err) return res.sendStatus(403)
+  })
+
+  await avoirTagsId(`${req.params.id}`).then((r) => {
+    res.json(r);
+  });
 });
 
 // Start the server
